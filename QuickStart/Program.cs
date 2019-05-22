68d20f9eb785c41acc3262046b4d473a1f0f417a
Rebecca Mills <beccamills86@gmail.com>
2019-05-22 17:52:39 -0230

    * Program.cs: cleaned up code
    * users.cql: added

--- QuickStart/Program.cs
+++ QuickStart/Program.cs
@@ -8,7 +8,7 @@ namespace QuickStart
     {
         static void Main(string[] args)
         { 
-            // Connect to the "demo" keyspace on our cluster running at 127.0.0.1
+
             Cluster cluster = Cluster.Builder().AddContactPoint("127.0.0.1").Build();
             ISession session = cluster.Connect("demo");
 
@@ -23,32 +23,29 @@ namespace QuickStart
             DeleteUser(session, "Jones");
         }
 
-        private static void SetUser(ISession session, String lastname, int age, String city, String email, String firstname)
-        {
+        private static void SetUser(ISession session, String lastname, int age, String city, String email, String firstname) {
             var statement = new SimpleStatement("INSERT INTO users (lastname, age, city, email, firstname) VALUES (?,?,?,?,?)", lastname, age, city, email, firstname);
 
             session.Execute(statement);
 
         }
 
-        private static void GetUser(ISession session, String lastname)
-        {
+        private static void GetUser(ISession session, String lastname){
+
             var statement = new SimpleStatement("SELECT * FROM users WHERE lastname = ?", lastname);
             var result = session.Execute(statement).First();
             Console.WriteLine("{0} {1}", result["firstname"], result["age"]);
 
         }
        
-        public static void UpdateUser(ISession session, int age, String lastname)
-        {
+        private static void UpdateUser(ISession session, int age, String lastname) {
 
             var statement = new SimpleStatement("UPDATE users SET age =? WHERE lastname = ?", age, lastname);
 
             session.Execute(statement);
         }
 
-        public static void DeleteUser(ISession session, String lastname)
-        {
+        private static void DeleteUser(ISession session, String lastname) {
 
             var statement = new SimpleStatement("DELETE FROM users WHERE lastname = ?", lastname);
 
